1a5da2eb39dea353941d217a1c41deebd38e91e8
Aleksandr Fedulov <AFedulov87@mail.ru>
2021-09-12 16:31:44 +0300

    First pgm

---
+++ src/main/CPP/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251>null");
+    cout<<"FFFEEEEDDDD"<<endl;
+    system("pause>null");  
+
+    return 0;
+}
